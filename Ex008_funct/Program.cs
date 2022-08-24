3fe5315a5df8898b20f8dc6e26de871a5f08a13d
maria <m.n.chernigov@gmail.com>
2022-08-24 12:21:10 +0700

    lec2 and sem2

---
+++ Ex008_funct/Program.cs
@@ -0,0 +1,106 @@
+﻿
+/*
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 5;
+int a2 = 10;
+int a3 = 50;
+int b1 = 200;
+int b2 = 100;
+int b3 = 60;
+
+// int max1 = Max(a1, a2, a3);
+// int max2 = Max(b1, b2, b3);
+// int max = Max(max1, max2, 0);
+
+int max = Max(Max(a1, a2, a3), Max(b1, b2, b3), 0);
+Console.WriteLine(max);
+*/
+
+/*
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int FindBiggerDigit(int number) // void - если нет return
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    int result;
+    if (ed > dec) result = ed;
+    else result = dec;
+
+    return result;
+}
+
+int randNumber = new Random().Next(10, 100);
+int result = FindBiggerDigit(randNumber);
+
+Console.WriteLine($"Bigger digit of {randNumber} is {result}");
+*/
+/*
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+string DelTwoNum(int number)
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    int cot = number / 100;
+    string res = Convert.ToString(cot) + Convert.ToString(ed);
+    return (res);
+}
+int randNumber = new Random().Next(100, 1000);
+
+string result = DelTwoNum(randNumber);
+Console.WriteLine($"случайное тречзначное число {randNumber}, результат задачи {result}");
+*/
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Если второе число не кратно числу первому, то программа выводит остаток от деления.
+/*
+void SecondMultipleFirst(int first, int second)
+{
+    int res = first % second;
+    if (res == 0) Console.WriteLine($" Второе число {second} кратно первому {first}");
+    else Console.WriteLine($"Остаток от деления равен {res}");
+}
+
+Console.WriteLine("Input first number: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+
+SecondMultipleFirst(first, second);
+*/
+/*
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+bool Multiple(int num, int a, int b)
+{
+    if (num / a == 0 & num / b == 0) return true;
+    else return false;
+}
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool res = Multiple(num, 7, 23);
+Console.WriteLine(res);
+
+*/
+//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+void Scware(int a, int b)
+{
+    int res1 = a * a;
+    int res2 = b * b;
+    if (res1 == b ^ res2 == a) Console.WriteLine("yes");
+    else Console.WriteLine("no");
+
+}
+
+Console.WriteLine("Input first number: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+Scware(first, second);
---
+++ Ex009_massiv/Program.cs
@@ -0,0 +1,71 @@
+﻿
+// int[] array = { 5, 19, 50, 20, 16, 80, 50, 26, 56 };
+// array[0] = 12;
+//Console.WriteLine(array[0]); показать первый элемент
+// int n = array.Length;
+// int find = 50;
+// int index = 0;
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
+
+
+/*
+// void если нет return
+void FillArray(int[] collection) //заполнить рандомно массив
+{
+    int len = collection.Length;
+    int index = 0;
+    while (index < len)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col) // напечатать заполненный массив
+{
+    int count = col.Length;
+    int posis = 0;
+    while (posis < count)
+    {
+        Console.Write(col[posis]);
+        posis++;
+    }
+}
+
+
+int IndexOf(int[] collec, int find)
+{
+    int count = collec.Length;
+    int index = 0;
+    int position = -1; // если число не встретится, то выводим -1
+
+    while (index < count)
+    {
+        if (collec[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+int pos = IndexOf(array, 4); // ищим на каком индексе 4
+Console.WriteLine();
+Console.WriteLine(pos);
+
+*/
